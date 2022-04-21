0e179cc91132c62106445dbb652d12da81914c63
eaparicio <erapariciopuerta@gmail.com>
2022-04-21 20:52:33 +0200

    refactoring pymiSRA to miSRA

--- django-server/miSRA/settings.py
+++ django-server/miSRA/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pymiSRA.urls'
+ROOT_URLCONF = 'miSRA.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pymiSRA.wsgi.application'
+WSGI_APPLICATION = 'miSRA.wsgi.application'
 
 
 # Database
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BACKEND_CONFIG = json.load("/shared/home/pymiSRA/config.json")
--- django-server/profiler/views.py
+++ django-server/profiler/views.py
@@ -8,36 +8,66 @@ import os
 import json
 import subprocess
 import shutil
-
+from django.conf.settings import BACKEND_CONFIG
+import string
 # from django.shortcuts import render
 # from django.forms import UploadFileForm
 
 
+def generate_uniq_id(size=15, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+#
+def handle_uploaded_file(f, outname):
+    with open(outname, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def writeConfigFile(data, files, configFile, configString):
+    """
+    The function writes out the config file in 'sRNAbench style' (parameter=value)
+    It does not return any value
+    """
+    f = open(configFile, "w")
+    for k in data:
+        f.write(k + "=" + data[k] + "\n")
+    for k in files:
+        f.write(k + "=" + files[k] + "\n")
+    f.write(configString + "\n")
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 
 def index(request):
 
-        configFile = "/shared/home/pymiSRA/config.json"
-        ##### Load the json file from the client
-        data={}        
-        if(os.path.isfile(configFile)):
-                print ("load config file")
-                data = getJson(configFile)
-        else:
-                print(configFile+" not found")
+        # configFile = "/shared/home/pymiSRA/config.json"
+        data = BACKEND_CONFIG
         # # # #
         ### prepare the temporary files
         outbase = data["outbase"]
-        rand = "id"+str(int(1000000*random.random()))
+        # rand = "id"+str(int(1000000*random.random()))
+        rand = "id" + generate_uniq_id()
         userOutBase = outbase +"/"+ rand
         os.mkdir(userOutBase)
         clientID = get_client_ip(request)
         ## write the temorary files into the temp folder
         localFiles={}
-        for k in request.FILES:
-                print ("go for "+k)
-                tmpFile=userOutBase+"/"+k+".fa"
-                handle_uploaded_file(request.FILES[k],tmpFile)
-                localFiles[k]=tmpFile
+        if request.FILES:
+            for k in request.FILES:
+                    print ("go for "+k)
+                    tmpFile=userOutBase+"/"+k+".fa"
+                    handle_uploaded_file(request.FILES[k],tmpFile)
+                    localFiles[k]=tmpFile
+        else:
+            localFiles["test"] = data.get("test_fasta_file")
                 
         ## write the config file in sRNAbench style
         configFile = userOutBase+"/config.txt"
@@ -54,13 +84,12 @@ def index(request):
         shutil.make_archive(userOutBase, 'zip', userOutBase )
         
 
-        try:    
-                with open(zipfile, 'rb') as f:
-                        file_data = f.read()
-
-        # sending response 
+        try:
+            with open(zipfile, 'rb') as f:
+                file_data = f.read()
+                # sending response
                 response = HttpResponse(file_data, content_type='application/zip')
-                response['Content-Disposition'] = 'attachment; filename='+zipfile
+                response['Content-Disposition'] = 'attachment; filename=' + zipfile
 
         except IOError:
         # handle file not exist case here
@@ -68,37 +97,18 @@ def index(request):
 
         return response
 
-def getJson(file):
-   #check if json is valid
-    try:
-        f = open(file,"r")
-        data = json.load(f)
-        return data
-    except ValueError as e:
-        print(e)
-        print("Errors in json format!")
+# def getJson(file):
+#    #check if json is valid
+#     try:
+#         f = open(file,"r")
+#         data = json.load(f)
+#         return data
+#     except ValueError as e:
+#         print(e)
+#         print("Errors in json format!")
+#
+#     return {}
 
-    return {}
-
-def writeConfigFile(data,files,configFile, configString):
-        """
-        The function writes out the config file in 'sRNAbench style' (parameter=value)
-        It does not return any value
-        """
-        f=open(configFile,"w")
-        for k in data:
-                f.write(k+"="+data[k]+"\n")
-        for k in files:
-                f.write(k+"="+files[k]+"\n")
-        f.write(configString+"\n")
-        
-def get_client_ip(request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-                ip = x_forwarded_for.split(',')[0]
-        else:
-                ip = request.META.get('REMOTE_ADDR')
-        return ip
 
 
 # def upload_file(request): 
@@ -113,7 +123,3 @@ def get_client_ip(request):
 # #     return render(request, 'upload.html', {'form': form})
 #         return False
 
-def handle_uploaded_file(f, outname):
-        with open(outname, 'wb+') as destination:
-                for chunk in f.chunks():
-                        destination.write(chunk)
