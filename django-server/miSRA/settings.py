7ed0ce29f2f668fe6b1cfa938629eed1cdae8976
eaparicio <erapariciopuerta@gmail.com>
2022-04-21 21:05:26 +0200

    refactoring pymiSRA to miSRA

--- django-server/miSRA/settings.py
+++ django-server/miSRA/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import json
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,5 +125,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CONFIG_JSON_PATH = os.path.join(BASE_DIR, "config.json")
 
-BACKEND_CONFIG = json.load(open("/shared/home/pymiSRA/config.json"))
+BACKEND_CONFIG = json.load(open(CONFIG_JSON_PATH))
